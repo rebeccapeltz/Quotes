533745143cb5c420db4e35cf55147032e2f0ef03
Aric Masih <masiharic@seattleu.edu>
2019-08-08 18:28:33 -0700

    Got data displaying

--- src/components/QuoteofDay.vue
+++ src/components/QuoteofDay.vue
@@ -1,23 +1,38 @@
 
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>2 Things that are difficult in JavaScript</h2>
-    <!-- <ol>
-      <li>naming things</li>
-      <li>recursion</li>
-      <li>off-by-one errors</li>
-    </ol> -->
+  <div class="quoteofday">
+    <h1>Quote of the Day!</h1>
+    <div v-if="posts && posts.quote">
+      <h2>{{ posts.quote.body }}</h2>
+      <h3>{{posts.quote.author}}</h3>
+    </div>
+    <form v-on:submit.prevent="getQuote(query)"></form>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'QuoteofDay',
-  props: {
-    msg: String
+  name: "quoteofDay",
+  data() {
+    return {
+      msg: "Quote of Day",
+      posts: null
+    };
+  },
+  created() {
+    axios
+      .get(` https://favqs.com/api/qotd`)
+      .then(response => {
+        this.posts = response.data;
+        console.log(this.posts);
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
