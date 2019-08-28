a40536ab93d85ae91fa4b6495527f4019f1e9f5d
rebeccapeltz <rebeccapeltz@gmail.com>
2019-08-28 12:07:39 -0700

    use fetch api to run

--- src/components/SearchQuote.vue
+++ src/components/SearchQuote.vue
@@ -5,11 +5,12 @@
         <h3>You can search for a Quote from an Author!</h3>
         <br />
         <label>Enter Author name here:</label>
-        <input type="search" v-model="body" placeholder="eg. Mark Twain" />
+        <input type="search" v-model="author" placeholder="eg. Mark Twain" />
         <button type="submit">Search</button>
         <button type="reset">Clear</button>
       </div>
     </form>
+    {{this.results}}
   </div>
 </template>
 
@@ -18,26 +19,33 @@ import axios from "axios";
 
 export default {
   name: "searchQuote",
-  data() {
+  data () {
     return {
       author: null,
-      body: null
+      body: null,
+      results:null
     };
   },
   methods: {
-    findQuote: function() {
-      let params = { "filter": "Mark Twain", "type": "author" };
-      let headers = {
-        "Authorization": "Token token='533e74f5138651604d3a8a30491dca5c'"
-      };
-      axios
-        .get("https://favqs.com/api/quotes/", headers, params)
-        .then(function(response) {
-          console.log(response.data);
+    findQuote: function () {   
+      let author = encodeURI(this.author);
+      fetch(`https://favqs.com/api/quotes?type=author&filter=${author}`,
+        {
+          headers: {
+            'Authorization': 'Token token="533e74f5138651604d3a8a30491dca5c"'
+          }
         })
-        .catch(function(error) {
+        .then(response=> {
+          return response.json();
+        })
+        .then(data => {
+          this.results = data;
+          console.log(JSON.stringify(data));
+        })
+        .catch(function (error) {
           console.log(error);
         });
+
     }
   }
 };
