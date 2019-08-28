53976f561e17d838393489e6a5786173173b1d9e
Aric Masih <masiharic@seattleu.edu>
2019-08-27 18:47:43 -0700

    Second page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <QuoteofDay msg="Quote of the Day!"/>
+    <!-- <QuoteofDay msg="Quote of the Day!"/> -->
     <router-view/>
   </div>
 </template>
--- src/components/SearchQuote.vue
+++ src/components/SearchQuote.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="searchQuote">
-    <h3>Search for a Quote:</h3>
-    <p>
-      <strong>{{author}}</strong>
-    </p>
-    <p>
-      <strong>{{body}}</strong>
-    </p>
+    <form v-on:submit.prevent="findQuote">
+      <div>
+        <h3>You can search for a Quote from an Author!</h3>
+        <br />
+        <label>Enter Author name here:</label>
+        <input type="search" v-model="body" placeholder="eg. Mark Twain" />
+        <button type="submit">Search</button>
+        <button type="reset">Clear</button>
+      </div>
+    </form>
   </div>
 </template>
 
-
 <script>
+import axios from "axios";
+
 export default {
   name: "searchQuote",
   data() {
@@ -20,9 +24,21 @@ export default {
       body: null
     };
   },
-  created: function() {
-    this.author = this.posts.quote.author;
-    this.body = this.posts.quote.body;
+  methods: {
+    findQuote: function() {
+      let params = { "filter": "Mark Twain", "type": "author" };
+      let headers = {
+        "Authorization": "Token token='533e74f5138651604d3a8a30491dca5c'"
+      };
+      axios
+        .get("https://favqs.com/api/quotes/", headers, params)
+        .then(function(response) {
+          console.log(response.data);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
   }
 };
 </script>
